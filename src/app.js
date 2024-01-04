e890dcdbf77441bc2817ec0855d198065e3ed556
Assaf <1214assaf@gmail.com>
2024-01-04 18:06:28 +0200

    Update .gitignore and package.json, add routing to app.js, and update webpack.config.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expanse component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expanse component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help component
+    </div>
+);
+
+const routes = (
+    <Router>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
