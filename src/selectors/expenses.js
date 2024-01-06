ad9d467bd1146cb10c20377623d4bfa814125d3d
Assaf <1214assaf@gmail.com>
2024-01-06 17:39:50 +0200

    Refactor ExpenseListFilters component and add date range picker

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,11 @@
+import moment from 'moment';
+
 // Get visible expenses
 export default (expenses, {text, sortBy, startDate, endDate}) => {
     return expenses.filter((expense) => {
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof startDate !== 'number' || expense.createdAt <= endDate;
+        const createdAtMoment = moment(expense.createdAt); // convert expense.createdAt to a moment object
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true; // if startDate exists, check if it is same or before the expense.createdAt date
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true; // if endDate exists, check if it is same or after the expense.createdAt date
         const textMatch = typeof text !== 'string' || expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
