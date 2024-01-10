0dfd579f0d29d8990ed50529d74646a4ccb6a72d
Assaf <1214assaf@gmail.com>
2024-01-10 19:43:07 +0200

    Refactor expenses.test.js to import expenses from fixtures

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,25 +1,6 @@
 import selectExpenses from '../../selectors/expenses';
 import moment from 'moment';
-const expenses = [{
-    id: '1',
-    description: 'Gum',
-    note: '',
-    amount: 195,
-    createdAt: 0
-}, {
-        id: '2',
-        description: 'Rent',
-        note: '',
-        amount: 109500,
-        createdAt: moment(0).subtract(4, 'days').valueOf() // subtract 4 days from the createdAt date
-}, {
-        id: '3',
-        description: 'Credit Card',
-        note: '',
-        amount: 4500,
-        createdAt: moment(0).add(4, 'days').valueOf() // add 4 days to the createdAt date
-
-}]
+import { expenses } from '../fixtures/expenses';
 
 test('should filter by text value', () => {
     const filters = {
